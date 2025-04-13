aa848f643f0b3c22fe594b78f5b28578ffa59812
Jayaprakash03042005 <jayajayaprakash331@gmail.com>
2025-04-13 14:57:45 +0530

    todo list

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const todoRouter=require('./routes/todo');
+
+
+const app=express();    
+const port=3000;
+
+
+app.use('/todo',todoRouter);
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
